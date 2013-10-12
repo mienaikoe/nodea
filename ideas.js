8c9d10f25ea1aad6cc896bcdc702e822dd8011cf
Jesse <mienaikoe@mienaikoe-VirtualBox.(none)>
2013-10-12 00:59:11 -0400

    First Pages Commit

---
+++ ideas.js
@@ -0,0 +1,199 @@
+var Ideas = function() {
+    
+    this.container = $("#ideas");
+    
+    this.setSliverTiming();
+    
+    this.startTime = null;
+    this.recording = false;
+    this.recordingNotes = {};
+    
+    this.tracksContainer = this.container.children("#tracks");
+
+    for( _i in project.timings ){
+        this.createNoteContainer(project.timings[_i]);
+    }
+            
+    var barsContainer = this.container.children("#barlines");        
+    for( var _i=0; _i < project.numBeats; _i++  ){
+        jQuery('<div/>',{class: 'beat'}).prependTo(barsContainer);
+    }
+    
+    var containerHeight = (project.numBeats*this.sliversPerBeat)+1;    
+    this.maxBottom = $('#circuit').outerHeight();
+    this.minBottom = this.maxBottom - containerHeight;
+    this.container.css('height', containerHeight+'px' ).css('bottom', this.maxBottom+'px');
+};
+
+Ideas.prototype.framesPerSecond = 20;
+
+
+
+// Calculation
+Ideas.prototype.setSliverTiming = function(){
+    this.sliversPerBeat = (192 / project.beat); // 48 for a quarter note, 24 for an eight note, ...
+    this.sliversPerSecond = (project.bpm * this.sliversPerBeat) / 60;
+};
+
+
+
+
+
+// Recording
+Ideas.prototype.toggleRecording = function(){
+    this.recording = !this.recording;
+    if( this.recording === false ) {
+        for( _i in this.recordingNotes ){
+            this.recordingNotes[_i].noda.turnOffPassiveRecording();
+        }
+        this.recordingNotes = {};
+    }
+    $('#controls #record').toggleClass("active");
+};
+
+Ideas.prototype.noteOn = function( noda ){
+    var note = {key: noda.key, on: this.sliverFor(Date.now()), noda: noda};
+    this.recordingNotes[noda.key] = note;
+    this.createNoteContainer(note);
+};
+
+Ideas.prototype.noteOff = function( noda ){
+    var note = this.recordingNotes[noda.key];
+    if( typeof note === 'undefined' ){ 
+        return;
+    }
+    
+    var thisSliver = this.sliverFor(Date.now());
+    if( note.on === thisSliver ){
+        note.container.remove();
+    } else {
+        note.off = thisSliver;
+        note.container.css('height',(note.off-note.on)+'px');
+        note.noda.addNote(note);
+    }
+
+    delete this.recordingNotes[noda.key];
+};
+
+Ideas.prototype.createNoteContainer = function(note){
+    if( assert(note) ){
+        var clazz = 'note';
+        if( typeof note.off === 'undefined' ){
+            note.off = note.on+1;
+            clazz = 'note recording';
+        } 
+        var slivers = note.off - note.on;
+        note.container = jQuery('<div/>',{
+            class: clazz,
+            style: 'bottom: '+note.on+'px; height: '+slivers+'px;'
+        }).prependTo(this.tracksContainer.children('#track_'+note.key.charCodeAt(0)));
+    }
+};
+
+Ideas.prototype.removeNoteContainer = function(note){
+    note.container.remove();
+};
+
+
+
+
+
+
+
+
+// Playback
+
+Ideas.prototype.constructPlayIntervalFxn = function( ){
+    var ides = this;
+    return function(){ ides.frame(); };
+};
+
+Ideas.prototype.start = function(){
+    // schedule all notes to play
+    this.setSliverTiming();
+    this.lastFrameSliver = this.currentSliver();
+    for( var _i in nodas ){
+        nodas[_i].startSources( this.sliversPerSecond, this.lastFrameSliver );
+    }
+    this.startTime = Date.now() - (this.lastFrameSliver / (this.sliversPerSecond/1000));
+    this.playInterval = window.setInterval(this.constructPlayIntervalFxn(), 1000/this.framesPerSecond);
+};
+
+Ideas.prototype.pause = function(){
+    this.startTime = null;
+    clearInterval(this.playInterval);
+    nodas.map(function(noda){ noda.stopSources(); });
+    for( var key in this.recordingNotes ){
+        var note = this.recordingNotes[key];
+        note.noda.turnOffPassiveRecording();
+        if( note.container ){
+            note.container.css('height',note.off-note.on+'px');
+        }
+    }
+};
+
+Ideas.prototype.playpause = function(){
+    $('#controls #playpause').toggleClass("active");
+    if( this.startTime === null ){ 
+        this.start(); 
+    } else { 
+        this.pause(); 
+    }
+};
+
+
+
+Ideas.prototype.currentSliver = function(){
+    return Math.ceil( this.maxBottom - parseFloat(this.container.css('bottom')) );
+};
+
+Ideas.prototype.sliverFor = function(epoch){
+    if( this.startTime === null ){
+        return this.currentSliver();
+    } else {
+        return Math.ceil( (epoch - this.startTime) * this.sliversPerSecond / 1000 );
+    }
+};
+
+Ideas.prototype.frame = function(){
+    if( this.startTime === null ){
+        return this.pause();
+    }
+    
+    var currBott = parseFloat(this.container.css('bottom'));
+    if( currBott <= this.minBottom ){
+        return this.pause();
+    } 
+      
+    var sliver = this.sliverFor(Date.now());
+    this.container.css('bottom', this.maxBottom-sliver+'px');
+    
+    // handle lighting
+    for( _i in project.timings ){
+        var note = project.timings[_i];
+        if( note.on <= sliver && note.on > this.lastFrameSliver ){
+            note.noda.lightOn('active');
+        } else if( note.off <= sliver && note.off > this.lastFrameSliver ){
+            note.noda.lightOff('active');
+        }
+    }
+    for( _j in this.recordingNotes ){
+        var note = this.recordingNotes[_j];
+        if( note.container ){
+            note.container.css('height', sliver-note.start+'px');
+        }
+    }
+    
+    this.lastFrameSliver = sliver;
+};
+
+Ideas.prototype.reset = function(){
+    this.pause();
+    this.container.css('bottom',this.maxBottom+'px');
+};
+
+// not sure you'd ever need this...
+Ideas.prototype.end = function(){
+    this.pause();
+    this.container.css('bottom', this.minBottom+'px');
+};
---
+++ noda.js
@@ -0,0 +1,163 @@
+var Noda = function(noda, swytche, ctx, notes, bufferUrl) {
+    this.noda = noda;
+    this.swytche = swytche;
+    this.key = $(swytche).text();
+    this.context = ctx;
+    
+    this.notes = notes;
+    for( var ni in notes ){
+        notes[ni].noda = this;
+    }
+    
+
+    if (!bufferUrl) {
+        return;
+    } 
+    var thisNoda = this;
+    var request = new XMLHttpRequest();
+    request.open("GET", bufferUrl, true);
+    request.responseType = "arraybuffer";
+    request.onload = function() {
+        thisNoda.context.decodeAudioData(
+            request.response,
+            function(buffer) { 
+                thisNoda.buffer = buffer; 
+                thisNoda.resetSources();
+            },
+            function(buffer) { console.log("Error decoding sample for "+bufferUrl); }
+        );
+    };
+    request.send();
+};
+
+Noda.prototype.addNote = function(note){
+    if( note !== null ){
+        note.source = this.allocateSource();
+        this.notes.push(note);
+        project.timings.push(note);
+    }
+};
+
+
+Noda.prototype.allocateSource = function(){
+    var src = this.context.createBufferSource();
+    src.buffer = this.buffer;
+    src.connect(this.context.destination);
+    src.onended = function(){console.log('ended');};
+    return src;
+};
+
+Noda.prototype.deallocateSource = function(src){
+    src.stop(0);
+    src.disconnect(0);
+};
+
+
+
+
+
+// playback
+
+Noda.prototype.startSources = function(sliversPerSecond, startingAt){
+    var startTime = this.context.currentTime;
+    this.notes.map( function(note){
+        if( note.on >= startingAt ){
+            note.source.start((note.on/sliversPerSecond)+startTime);
+            note.source.stop((note.off/sliversPerSecond)+startTime);
+        }
+    });
+};
+
+Noda.prototype.stopSources = function(){
+    for( var _i in this.notes ){
+        var note = this.notes[_i];
+        this.deallocateSource(note.source);
+        note.source = this.allocateSource();
+    }
+    this.lightOff('active');
+};
+
+Noda.prototype.resetSources = function(){
+    for( var _i in this.notes ){
+        this.notes[_i].source = this.allocateSource();
+    }
+};
+
+
+
+
+
+// recording
+
+Noda.prototype.on = function() {
+    // if recording, notify ideas of new note.
+    if (this.buffer && !this.src) {
+        this.src = this.allocateSource();
+        this.src.start(0);
+        
+        if( ideas.recording ){
+            if( ideas.startTime !== null ){ //active recording
+                ideas.noteOn(this);
+                this.lightOn('recording');
+            } else { // passive recording
+                if( this.passiveRecording ){
+                    this.turnOffPassiveRecording();
+                } else {
+                    ideas.noteOn(this);
+                    this.passiveRecording = true;
+                    this.lightOn('recording');
+                }
+            }
+            
+        } else {
+            this.lightOn('active');
+        }
+    }
+};
+
+
+Noda.prototype.turnOffPassiveRecording = function(){
+        ideas.noteOff(this);
+        this.passiveRecording = false;
+        this.lightOff('recording');
+};
+
+
+
+Noda.prototype.off = function() {
+    if (this.src) {
+        this.deallocateSource(this.src);
+        this.src = null;
+        
+        if( ideas.recording ){
+            if( ideas.startTime !== null ){ //active recording
+                ideas.noteOff(this);
+                this.lightOff('recording');
+            }
+        } else {
+            this.lightOff('active');
+        }
+    }
+};
+
+
+
+
+
+
+
+// lighting
+
+Noda.prototype.lightOn = function(lightType){
+    $(this.noda).addClass(lightType);
+    $(this.swytche).addClass(lightType);
+};
+Noda.prototype.lightOff = function(lightType){
+    $(this.noda).removeClass(lightType);
+    $(this.swytche).removeClass(lightType);
+};
+
+Noda.prototype.lightsOut = function(){
+    $(this.noda).removeClass('active').removeClass('recording');
+    $(this.swytche).removeClass('active').removeClass('recording');
+};
