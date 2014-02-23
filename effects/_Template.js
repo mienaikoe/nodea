8e614a6559638a89e24791db34d089e87ec3636e
Jesse Lee <mienaikoe@gmail.com>
2014-02-23 11:21:21 -0800

    Refurbished Effects Template

--- effects/_Template.js
+++ effects/_Template.js
@@ -1,29 +1,53 @@
+/*
+ * Large parts of this file are borrowed from
+ * http://secretfeature.com/mono-synth/part3/scripts/MonoSynth.js
+ * 
+ */
 
-function Template(ctx, effectReplacementCallback) {
+function Envelope(ctx, effectReplacementCallback) {
 	this.ctx = ctx;
 	this.effectReplacementCallback = effectReplacementCallback;
 
-	this.input = null;
-	this.output = null;
-	// These are used for various connectivity things. Please Fill these in with something.
+	this.input = null; // this is required or else everything breaks.
+	this.output = this.input;
+
 }
 
 
-
-
-Template.prototype.render = function(division) {
-
+Envelope.prototype.render = function(division) {
+	// Render the UI for your Effect Here
 };
 
 
 
 
+/*
+ * Start / Stop
+ * @param when is the audio API timing for starting nodes.
+ * @return is the amount of time you want to delay the connecting ciruits. 
+ *   This is useful for instances where the effect will only work if the 
+ *   source continues for a certain time (i.e. release effects or 100% wet delays).
+ */
 
-
-Template.prototype.start = function(now) {
+Envelope.prototype.start = function(when) {
 	return 0;
 };
 
-Template.prototype.stop = function(now) {
+Envelope.prototype.stop = function(when) {
 	return 0;
+};
+
+
+
+// marshal / load
+Envelope.prototype.marshal = function() {
+	var ret = {};
+	// fill in ret here. You will load what this creates in the load function.
+	return ret;
+};
+
+Envelope.prototype.load = function(settings){
+	for( envParam in settings ){
+		this[envParam] = settings[envParam];
+	}
 };
