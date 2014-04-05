a86a696d44cbcb1fde424fa03068c41fcfe1b5d4
Jesse Lee <mienaikoe@gmail.com>
2014-04-05 14:26:22 -0400

    Notes can Move around

--- core/Note.js
+++ core/Note.js
@@ -4,6 +4,8 @@ var Note = function(options){
 	if( options.noda ){ this.noda = options.noda; } 
 };
 
+Note.EXPANDER_HEIGHT = 4;
+
 Note.prototype.createContainer = function(){
 	var clazz = '';
 	if( typeof this.finish === 'undefined' ){
@@ -12,12 +14,35 @@ Note.prototype.createContainer = function(){
 	} 
 	var slivers = this.finish - this.start;
 	var self = this;
-	this.container = jQuery('<div/>',{ class: 'note ' + clazz, style: 'bottom: '+this.start+'px; height: '+slivers+'px;'}).
-			mousedown(function(){ 
-				$(".note.selected").removeClass("selected"); 
+	
+	this.container = jQuery('<div/>',{ class: 'noteCapsule ' + clazz, style: 'bottom: '+(this.start-Note.EXPANDER_HEIGHT)+'px; height: '+(slivers+(Note.EXPANDER_HEIGHT*2))+'px;'}).
+			mousedown(function(ev){
+				$(".noteCapsule.selected").removeClass("selected"); 
 				$(this).addClass("selected");
 				studio.selectedNote = self;
 			});
+	
+	jQuery('<div/>',{ class: 'noteExpander north'}).appendTo(this.container);
+	
+	this.noteBox = jQuery('<div/>',{ class: 'note', style: 'height: '+slivers+'px;'}).
+			mousedown(function(ev){
+				if( self.container.hasClass("selected") ){
+					self.container.addClass("dragging");
+					self.mouseSetpoint = parseInt(self.container.css('bottom')) + ev.pageY;
+					self.container.mousemove(function(ev_move){
+						$(this).css('bottom', (self.mouseSetpoint - ev_move.pageY)+'px');
+					});
+				}
+			}).mouseup(function(ev){
+				if( self.container.hasClass("dragging") ){
+					self.move(self.mouseSetpoint - ev.pageY);
+					self.container.removeClass("dragging");
+				}
+			});
+			
+	this.noteBox.appendTo(this.container);
+	
+	jQuery('<div/>',{ class: 'noteExpander south'}).appendTo(this.container);
 			
 	if( this.noda ){
 		this.container.prependTo(this.noda.trackline);
@@ -33,4 +58,14 @@ Note.prototype.removeContainer = function(){
 Note.prototype.turnOffRecording = function(){
 	this.noda.turnOffPassiveRecording();
 	this.container.css('height', (this.finish-this.start) + 'px');
+};
+
+Note.prototype.move = function( newStart ){
+	this.container.css('bottom', (newStart)+'px');
+	this.container.unbind("mousemove");
+	
+	this.finish = this.finish + (newStart - this.start);
+	this.start = newStart;
+	
+	studio.invalidateSavedStatus();
 };
